e8b06891a9e2fdc62ed76e06fbeacb7d4f3f3d12
Nithin Jino <nithjino1995@gmail.com>
2020-01-30 20:26:53 -0500

    master: dynamic way to generate a thread for each group

--- src/bot_start.py
+++ src/bot_start.py
@@ -25,67 +25,36 @@ except KeyError:
     except FileNotFoundError:
         yt_key = None
 
+try:
+    with open(os.path.abspath(bot_path + '/../groups.txt'), 'r') as bot_groups:
+        groups = bot_groups.readlines()
+except FileNotFoundError:
+    print("Couldn't find  groups.txt. Make sure it is in the root of this directory")
+    sys.exit(1)
+
 client = Client.from_token(chat_key)
 
-# Use this for loop to find groups you want
-for g in client.groups.list():
-    if g.name == 'ddgbot test':
-        test_group = g
-    if g.name == 'Smurf City Pt 2':
-        smurf_city = g
-    if g.name == 'Couch':
-        house_hunters = g
 
-# make a bot object for each group you want
-smurf_city_bot = Bot(smurf_city, yt_key=yt_key)
-test_group_bot = Bot(test_group, yt_key=yt_key)
-house_hunters_bot = Bot(house_hunters, yt_key=yt_key)
+def consume(bot, s=1):
+    message = bot.get_message()
+    bot.process_message(message)
+    sleep(s)
+
+
+for g in groups:
+    try:
+        g = list(filter(lambda x: x.name == g.strip(), client.groups.list()))[0]
+        b = Bot(g, yt_key=yt_key)
+        threading.Thread(target=consume, daemon=True, args=(b, .1)).start()
+    except IndexError:
+        print("'{}' group doesn't exist".format(g))
 
 
 def shutdown_bot(signal, frame):
     print("Shutting down the bot")
-    smurf_city_bot.save_tags()
-    test_group_bot.save_tags()
-    house_hunters_bot.save_tags()
     sys.exit(0)
 
 
-def tag_reloader(signal, frame):
-    print("reloading tags")
-    smurf_city_bot.reload_tags()
-    test_group_bot.reload_tags()
-    house_hunters_bot.reload_tags()
-
-
 signal.signal(signal.SIGINT, shutdown_bot)
-signal.signal(signal.SIGQUIT, tag_reloader)
 
 print("Bot started up successfully")
-
-
-# make a method for each group
-def test_group():
-    while True:
-        message = test_group_bot.get_message()
-        test_group_bot.process_message(message)
-        sleep(1)
-        
-
-def smurf_group():
-    while True:
-        message = smurf_city_bot.get_message()
-        smurf_city_bot.process_message(message)
-        sleep(.1)
-
-
-def house_group():
-    while True:
-        message = house_hunters_bot.get_message()
-        house_hunters_bot.process_message(message)
-        sleep(.1)
-
-
-# add a threading method for each group
-threading.Thread(target=test_group, daemon=True).start()
-threading.Thread(target=smurf_group, daemon=True).start()
-threading.Thread(target=house_group, daemon=True).start()
