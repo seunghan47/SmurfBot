dfe162816d5d37b632289ec3665a14ea32c66e9f
Nithin Jino <nithjino1995@gmail.com>
2020-02-05 12:44:09 -0500

    master: changed json tag object to include group id and group name. tags is put into a dictionary value to the tags key

--- src/tags.py
+++ src/tags.py
@@ -1,3 +1,4 @@
+"""Handles all the tag related commands"""
 import json
 import os
 from functools import reduce
@@ -6,8 +7,15 @@ from atomicwrites import atomic_write
 
 
 class Tags:
-    def __init__(self, group_name, members):
+    """Handles all the tag related commands"""
+    def __init__(self, group_name, group_id, members):
+        """
+        :param group_name: name of the group
+        :param group_id: id of the group
+        :param members: list of members of the group
+        """
         self.group_name = group_name
+        self.group_id = group_id
         self.members = members
         self.bot_path = os.path.dirname(os.path.realpath(__file__))
         self.create_json(self.group_name)
@@ -29,7 +37,7 @@ class Tags:
             print("created: {}".format(tag_folder))
 
         if not os.path.exists(tag_json):
-            create_json = {'tags': []}
+            create_json = {'name': self.group_name, 'id': self.group_id, 'tags': {}}
             with open(tag_json, 'w') as tags:
                 json.dump(create_json, tags)
             print("created: {}".format(tag_json))
@@ -41,11 +49,11 @@ class Tags:
             return json.load(tags)
 
     def reload_tags(self):
-        print("reloading tags")
+        print("reloading tags of {}".format(self.group_name))
         self.tags = self.load_tags()
 
     def update_members(self, members):
-        print("updating members")
+        print("updating members of {}".format(self.group_name))
         self.members = members
 
     def save_tags(self):
@@ -94,47 +102,45 @@ class Tags:
         return tag_result
 
     def create_tag(self, name, content, owner):
-        if name in self.tags:
+        if name in self.tags['tags']:
             return 'The tag "{}" already exists'.format(name)
-        self.tags[name] = {'owner': owner, 'content': content}
+        self.tags['tags'][name] = {'owner': owner, 'content': content}
         self.save_tags()
         return 'The tag "{}" was created successfully'.format(name)
 
     @staticmethod
     def get_tag_content(message, mentions):
-        if len(mentions) > 0:
+        if mentions:
             url = list(filter(lambda x: x.type == 'image' or x.type == 'video', mentions))
             return url[0].url
-        else:
-            return reduce((lambda x, y: x + " " + y), message)
+        return reduce((lambda x, y: x + " " + y), message)
 
     def post_tag(self, name):
         print("searching for tag: {}".format(name))
-        if name in self.tags:
-            tag = self.tags[name]['content']
+        if name in self.tags['tags']:
+            tag = self.tags['tags'][name]['content']
             if tag.split(" ")[0] == "$tag":
                 return self.post_tag(tag.split(" ")[1])
             return tag
         return 'The tag "{}" does not exist'.format(name)
 
     def delete_tag(self, name, owner):
-        if len(self.tags) == 0:
+        if not self.tags['tags']:
             return 'There are no tags'
         try:
-            if self.tags[name]['owner'] == owner:
-                del self.tags[name]
+            if self.tags['tags'][name]['owner'] == owner:
+                del self.tags['tags'][name]
                 self.save_tags()
                 return 'The tag "{}" has been deleted'.format(name)
-            else:
-                return 'You are not the owner of the tag "{}"'.format(name)
+            return 'You are not the owner of the tag "{}"'.format(name)
         except KeyError:
             return 'The tag "{}" does not exist'.format(name)
 
     def list_tags(self):
-        if len(self.tags) == 0:
+        if not self.tags['tags']:
             return 'No tags exist'
 
-        all_tags = ', '.join(self.tags.keys())
+        all_tags = ', '.join(self.tags['tags'].keys())
 
         if len(all_tags) > 1000:
             all_tags = list(map(''.join, zip_longest(*[iter(all_tags)] * 1000, fillvalue='')))
@@ -143,67 +149,59 @@ class Tags:
         return all_tags
 
     def edit_tag(self, name, owner, content):
-        if len(self.tags) == 0:
+        if not self.tags['tags']:
             return "There are no tags to edit"
         try:
-            if self.tags[name]['owner'] == owner:
-                self.tags[name]['content'] = content
+            if self.tags['tags'][name]['owner'] == owner:
+                self.tags['tags'][name]['content'] = content
                 self.save_tags()
                 return 'The tag "{}" has been edited'.format(name)
-            else:
-                return 'You are not the owner of the tag "{}"'.format(name)
+            return 'You are not the owner of the tag "{}"'.format(name)
         except KeyError:
             return 'The tag "{}" does not exist'.format(name)
 
     def rename_tag(self, old_name, new_name, owner):
-        if len(self.tags) == 0:
+        if not self.tags['tags']:
             return "There are no tags"
-        if new_name in self.tags:
+        if new_name in self.tags['tags']:
             return 'The tag "{}" already exists. Can\'t change name to it'.format(new_name)
         try:
-            if self.tags[old_name]['owner'] == owner:
-                self.tags[new_name] = {'owner': owner, 'content': self.tags[old_name]['content']}
-                del self.tags[old_name]
+            if self.tags['tags'][old_name]['owner'] == owner:
+                self.tags['tags'][new_name] = {'owner': owner, 'content': self.tags[old_name]['content']}
+                del self.tags['tags'][old_name]
                 self.save_tags()
                 return 'The tag "{}" has been renamed to "{}"'.format(old_name, new_name)
-            else:
-                return 'You are not the owner of the tag "{}"'.format(old_name)
+            return 'You are not the owner of the tag "{}"'.format(old_name)
         except KeyError:
             return 'The tag "{}" does not exist'.format(old_name)
 
     def gift_tag(self, name, owner, new_owner):
-        if len(self.tags) == 0:
+        if not self.tags['tags']:
             return "There are no tags"
         try:
-            if self.tags[name]['owner'] == owner:
-                self.tags[name]['owner'] = new_owner
+            if self.tags['tags'][name]['owner'] == owner:
+                self.tags['tags'][name]['owner'] = new_owner
                 self.save_tags()
                 return 'The tag "{}" has been gifted to "{}"'.format(name, new_owner)
-            else:
-                return 'You are not the owner of the tag "{}"'.format(name)
+            return 'You are not the owner of the tag "{}"'.format(name)
         except KeyError:
             return 'The tag "{}" does not exist'.format(name)
 
     def filter_tags(self, keyword):
-        tag_names = []
-        t = list(filter(lambda x: keyword in x, self.tags.keys()))
-
-        for tag in t:
-            tag_names.append(tag)
-
-        tag_names = ', '.join(tag_names)
-        if len(tag_names) == 0:
+        filtered_tags = list(filter(lambda x: keyword in x, self.tags['tags'].keys()))
+        filtered_tags = ', '.join(filtered_tags)
+        if not filtered_tags:
             return 'No tags contain the word {}'.format(keyword)
 
-        if len(tag_names) > 1000:
-            tag_names = list(map(''.join, zip_longest(*[iter(tag_names)] * 1000, fillvalue='')))
+        if len(filtered_tags) > 1000:
+            tag_names = list(map(''.join, zip_longest(*[iter(filtered_tags)] * 1000, fillvalue='')))
             return tag_names
 
-        return tag_names
+        return filtered_tags
 
     def find_owner(self, name):
-        if name in self.tags:
-            owner = self.find_owner_name(self.tags[name]['owner'])
+        if name in self.tags['tags']:
+            owner = self.find_owner_name(self.tags['tags'][name]['owner'])
             return "The owner of {} is {}".format(name, owner)
         return 'The tag "{}" does not exist'.format(name)
 
